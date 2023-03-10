a1e222b74e81dfc7eacae1773c21db2867b8957d
danielrebelo1 <daniel.rebelo2003@gmail.com>
2023-03-10 11:49:44 +0000

    updates on profile page

--- lib/profile-page.dart
+++ lib/profile-page.dart
@@ -8,6 +8,24 @@ class ProfilePage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Color.fromRGBO(6, 10, 43, 1),
       ),
+      body: ImageWidget(),
+    );
+  }
+}
+
+class ImageWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [Positioned(bottom: 0, left: 80, child: Image.asset(
+        'assets/logo.png',
+        width: 240.0, height: 240.0,
+      ),),
+        Positioned(top: 10,left: 0 , right: 0,
+            child: Container( height: 50,
+              child: Center(child: Text('PROFILE',style: TextStyle(color: Colors.white,fontSize: 50),),),
+            ))
+      ],
     );
   }
 }
